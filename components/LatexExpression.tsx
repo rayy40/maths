ca4fe1ab314b897669a644f711e45e2ad3fe8a70
Rayyan Alam <rayyanalam028@gmail.com>
2023-07-11 04:30:35 +0530

    Fixed stylings

--- components/LatexExpression.tsx
+++ components/LatexExpression.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import "katex/dist/katex.min.css";
+// import "katex/dist/katex.min.css";
 import { BlockMath } from "react-katex";
 
 type Props = {
--- components/RenderMatrix.tsx
+++ components/RenderMatrix.tsx
@@ -1,21 +1,30 @@
 import React from "react";
-import { BlockMath } from "react-katex";
+import { BlockMath, InlineMath } from "react-katex";
 import styles from "@/styles/matrix.module.css";
+import { useGlobalContext } from "@/context/store";
 
-type Props = {
-  matrix: string[][];
-};
-
-export default function RenderMatrix({ matrix }: Props) {
-  console.log(matrix);
+export default function RenderMatrix() {
+  const { matrixHistory, variable } = useGlobalContext();
 
   return (
     <div className={styles.matrix_display}>
-      <BlockMath
-        math={`\\begin{bmatrix}
-        ${matrix.map((row) => row.join(" & ")).join(" \\\\ ")}
-  \\end{bmatrix}`}
-      />
+      <div className={styles.matrix_equation}>
+        <InlineMath math={variable} />
+        <InlineMath math={"="} />
+        <div style={{ fontSize: "1.25rem" }}>
+          <BlockMath
+            math={`\\begin{bmatrix}
+            ${matrixHistory?.[variable]
+              ?.map((row) =>
+                row
+                  .map((value) => Math.round(parseFloat(value) * 1000) / 1000)
+                  .join(" & ")
+              )
+              .join(" \\\\ ")}
+            \\end{bmatrix}`}
+          />
+        </div>
+      </div>
     </div>
   );
 }
