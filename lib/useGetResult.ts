9a1fad22e102ad90d164f79745b26c3fec1336bc
Rayyan Alam <rayyanalam028@gmail.com>
2023-07-14 04:02:07 +0530

    Added getEquation function to get matrix equation result

--- lib/useGetResult.ts
+++ lib/useGetResult.ts
@@ -1,7 +1,12 @@
 import { useGlobalContext } from "@/context/store";
 
 const useGetResult = () => {
-  const { setIsLoading, setIsError, setMatrixHistory } = useGlobalContext();
+  const {
+    setIsLoading,
+    setIsError,
+    setMatrixHistory,
+    setLatexCalculatedResult,
+  } = useGlobalContext();
 
   const getResult = async (data: {
     matrix: { [key: string]: [][] } | string[][];
@@ -27,7 +32,7 @@ const useGetResult = () => {
         console.log(res);
         setMatrixHistory((prev) => ({
           ...prev,
-          [data.expression]: [...res.result],
+          [data.expression]: res.result,
         }));
         setIsLoading(false);
       } else {
@@ -42,7 +47,41 @@ const useGetResult = () => {
     }
   };
 
-  return { getResult };
+  const getEquation = async (data: {
+    matrixHistory: { [key: string]: string[][] };
+    equation: string;
+  }) => {
+    setIsLoading(true);
+
+    const options = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    };
+    try {
+      const response = await fetch(
+        `http://localhost:8000/api/matrix/equation`,
+        options
+      );
+      if (response.ok) {
+        const res = await response.json();
+        setLatexCalculatedResult(res.latex);
+        setIsLoading(false);
+      } else {
+        console.log("Error:", response.status);
+        setIsLoading(false);
+        setIsError(true);
+      }
+    } catch (error) {
+      console.log("Error:", error);
+      setIsLoading(false);
+      setIsError(true);
+    }
+  };
+
+  return { getResult, getEquation };
 };
 
 export default useGetResult;
