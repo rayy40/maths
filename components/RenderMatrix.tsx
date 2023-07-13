e335ad785a77c3818a80b437e50ff59aea980308
Rayyan Alam <rayyanalam028@gmail.com>
2023-07-14 04:03:37 +0530

    Fixed rendering for matrix as well as scalar

--- components/RenderMatrix.tsx
+++ components/RenderMatrix.tsx
@@ -2,10 +2,18 @@ import React from "react";
 import { BlockMath, InlineMath } from "react-katex";
 import styles from "@/styles/matrix.module.css";
 import { useGlobalContext } from "@/context/store";
+import { convertToLatex } from "@/lib/Helper";
 
 export default function RenderMatrix() {
   const { matrixHistory, exp, matrixEquation, isCalculatorOn } =
     useGlobalContext();
+
+  const containsLatexExpression = (exp: string) => {
+    const latexPattern = /\\[a-zA-Z]+\{[^\}]+\}|\\\(|\\\)|\\\[|\\\]/;
+    return latexPattern.test(exp);
+  };
+
+  console.log(typeof matrixHistory?.[exp]);
 
   return (
     <div className={styles.matrix_display}>
@@ -20,17 +28,11 @@ export default function RenderMatrix() {
         <div style={{ fontSize: "1.25rem" }}>
           {isCalculatorOn ? (
             <BlockMath math={matrixEquation} />
+          ) : typeof matrixHistory?.[exp] === "string" ? (
+            <BlockMath math={matrixHistory?.[exp] as string} />
           ) : (
             <BlockMath
-              math={`\\begin{bmatrix}
-            ${matrixHistory?.[exp]
-              ?.map((row) =>
-                row
-                  .map((value) => Math.round(parseFloat(value) * 1000) / 1000)
-                  .join(" & ")
-              )
-              .join(" \\\\ ")}
-            \\end{bmatrix}`}
+              math={convertToLatex(matrixHistory?.[exp] as string[][])}
             />
           )}
         </div>
