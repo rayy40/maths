6d3aa99ed3f4e5a3d853a8db9d004746e366d9cb
Rayyan Alam <rayyanalam028@gmail.com>
2023-07-16 03:33:33 +0530

    Refactored the calculation component with loading and error

--- components/RenderCalculation.tsx
+++ components/RenderCalculation.tsx
@@ -1,33 +1,38 @@
 import React from "react";
 import styles from "@/styles/matrix.module.css";
 import { BlockMath, InlineMath } from "react-katex";
-import { useGlobalContext } from "@/context/store";
+import { useMatrixContext } from "@/context/MatrixContext";
+import Loading from "./Loading";
+import { displayDifferentErrorMessage } from "@/lib/Helper";
 
-type Props = {
-  title: string;
-};
-
-export default function RenderCalculation({ title }: Props) {
-  const { matrixEquation, matrixHistory } = useGlobalContext();
-
-  console.log(matrixEquation);
-
-  const substitutedEquation = matrixEquation
-    .replace(/[{()}]/g, "")
-    .split(/(?=[+\-*/])/)
-    .map((term) => term.replace(/^[+\-]|(?<=\^)[+\-]$/g, ""));
-
-  console.log(substitutedEquation);
+export default function RenderCalculation() {
+  const { isLoading, isError, errorMessage, latexCalculatedResult } =
+    useMatrixContext();
 
   return (
-    <div className={styles.matrix_calculation_display}>
-      {title === "equation" && (
-        <div className={styles.matrix_equation}>
-          <InlineMath math={"Eq^n: "} />
-          <InlineMath math={matrixEquation} />
+    <div className={styles.calculation_wrapper}>
+      <h3 style={{ fontSize: "1.15rem" }} className={styles.sub_title}>
+        Calculation:
+      </h3>
+      {isError && (
+        <div className={styles.error_wrapper}>
+          <h3 style={{ fontWeight: 400, color: "#B43A4E", padding: "2em 0" }}>
+            {displayDifferentErrorMessage(errorMessage)}
+          </h3>
         </div>
       )}
-      {title === "values" && <div className={styles.matrix_equation}></div>}
+      {isLoading && (
+        <div className={styles.loading_wrapper}>
+          <Loading />
+        </div>
+      )}
+      {!isError && !isLoading && (
+        <div className={styles.matrix_equation}>
+          <InlineMath math={latexCalculatedResult.equation} />
+          <InlineMath math={"="} />
+          <BlockMath math={latexCalculatedResult.result} />
+        </div>
+      )}
     </div>
   );
 }
