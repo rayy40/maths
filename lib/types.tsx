1a5569cc0ac65d6ce189ec066eff0dbce503fda4
Rayyan Alam <rayyanalam028@gmail.com>
2023-07-16 14:09:12 +0530

    unnecessary component

--- lib/types.tsx
+++
@@ -1,9 +0,0 @@
-export const operations: { name: string; exp: string }[] = [
-  { name: "square", exp: "A^2" },
-  { name: "transpose", exp: "A^T" },
-  { name: "inverse", exp: "A^{-1}" },
-  { name: "power", exp: "A^n" },
-  { name: "ref", exp: "rref(A)" },
-  { name: "determinant", exp: "det(A)" },
-  { name: "trace", exp: "trace(A)" },
-];
