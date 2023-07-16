8b67161e0d32eb0493554568f1c7cec4ef58a8e8
Rayyan Alam <rayyanalam028@gmail.com>
2023-07-16 14:10:07 +0530

    check for only numbers to be enterted

--- components/Matrix.tsx
+++ components/Matrix.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "@/styles/matrix.module.css";
 
 type Props = {
@@ -7,6 +7,8 @@ type Props = {
 };
 
 export default function Matrix({ matrix, setMatrix }: Props) {
+  const [isInputError, setIsInputError] = useState(false);
+
   const handleMatrixData = (
     event: React.ChangeEvent<HTMLInputElement>,
     rowIndex: number,
@@ -14,30 +16,48 @@ export default function Matrix({ matrix, setMatrix }: Props) {
   ) => {
     const { value } = event.target;
 
-    setMatrix((prev) => {
-      const updatedData = [...prev];
-      updatedData[rowIndex][columnIndex] = value;
-      return updatedData;
-    });
+    if (!isNaN(Number(value)) || value === "-") {
+      setMatrix((prev) => {
+        const updatedData = [...prev];
+        updatedData[rowIndex][columnIndex] = value;
+        return updatedData;
+      });
+      setIsInputError(false);
+    } else {
+      setIsInputError(true);
+      // Display an error message or handle the error condition
+      console.error("Please enter a number.");
+    }
   };
 
   return (
-    <div className={styles.matrix_wrapper}>
-      {matrix.map((row, rowIndex) => (
-        <div key={rowIndex} className={styles.matrix_row}>
-          {row.map((val, columnIndex) => (
-            <input
-              key={columnIndex}
-              autoFocus={rowIndex === 0 && columnIndex === 0}
-              placeholder="0"
-              className={styles.matrix_input}
-              value={val}
-              type="text"
-              onChange={(e) => handleMatrixData(e, rowIndex, columnIndex)}
-            />
-          ))}
-        </div>
-      ))}
-    </div>
+    <>
+      <div
+        className={`${styles.matrix_wrapper} ${
+          isInputError && styles.matrix_wrapper_error
+        }`}
+      >
+        {matrix.map((row, rowIndex) => (
+          <div key={rowIndex} className={styles.matrix_row}>
+            {row.map((val, columnIndex) => (
+              <input
+                key={columnIndex}
+                autoFocus={rowIndex === 0 && columnIndex === 0}
+                placeholder="0"
+                className={`${styles.matrix_input} ${
+                  isInputError && styles.matrix_input_error
+                }`}
+                value={val}
+                type="text"
+                onChange={(e) => handleMatrixData(e, rowIndex, columnIndex)}
+              />
+            ))}
+          </div>
+        ))}
+      </div>
+      {isInputError && (
+        <p className={styles.input_error}>Please enter a number</p>
+      )}
+    </>
   );
 }
