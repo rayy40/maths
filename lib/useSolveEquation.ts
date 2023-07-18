2d947d946d905e6a3ec6d715c423a6c7d8ff0829
Rayyan Alam <rayyanalam028@gmail.com>
2023-07-19 00:56:37 +0530

    Made a hook to ftech results

---
+++ lib/useSolveEquation.ts
@@ -0,0 +1,46 @@
+import { useEquationSolverContext } from "@/context/EquationSolverContext";
+import { useApiContext } from "@/context/MatrixContext";
+
+const useSolveEquation = () => {
+  const { setIsError, setIsLoading, setErrorMessage } = useApiContext();
+  const { setReults } = useEquationSolverContext();
+
+  const getRoots = async (data: { equation: string }) => {
+    setIsLoading(true);
+
+    const options = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    };
+
+    try {
+      const response = await fetch(
+        `http://localhost:8000/api/roots/equation-solver`,
+        options
+      );
+      if (response.ok) {
+        const res = await response.json();
+        console.log(res);
+        setReults({ roots: res.roots, factors: res.factors });
+        setIsLoading(false);
+        setIsError(false);
+      } else {
+        const err = await response.json();
+        setIsError(true);
+        setIsLoading(false);
+        setErrorMessage(err.detail);
+      }
+    } catch (error) {
+      setIsError(true);
+      setErrorMessage((error as Error).message);
+      setIsLoading(false);
+    }
+  };
+
+  return { getRoots };
+};
+
+export default useSolveEquation;
