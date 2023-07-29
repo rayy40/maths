ca1c75c74164eaf8a47477c443fda263879c0da8
Rayyan Alam <rayyanalam028@gmail.com>
2023-07-29 14:21:10 +0530

    Added the inital page for geometry

--- app/geometry/page.tsx
+++ app/geometry/page.tsx
@@ -1,7 +1,31 @@
 import React from "react";
+import styles from "@/styles/geomtery.module.css";
+import { formula } from "@/lib/formulas";
+import Link from "next/link";
 
-type Props = {};
-
-export default function GeometryPage({}: Props) {
-  return <div>geometry page</div>;
+export default function GeometryPage() {
+  return (
+    <div className="page_container">
+      <div className={styles.geometry_outer_wrapper}>
+        {formula.map((shape) => (
+          <Link
+            className="link"
+            key={shape.id}
+            href={`/geometry/${shape.name}`}
+          >
+            <div className={styles.shape_container}>
+              <h3 className={styles.shape_name}>{shape.name}</h3>
+              <div
+                className={`${styles.shape_img} ${
+                  styles[`shape_img_${shape.id}`]
+                }`}
+              >
+                {shape.image}
+              </div>
+            </div>
+          </Link>
+        ))}
+      </div>
+    </div>
+  );
 }
