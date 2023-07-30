26f7d8dd4ec3f7091db596565f1a0273f90aafa3
Rayyan Alam <rayyanalam028@gmail.com>
2023-07-30 14:57:01 +0530

    fixed window is not defined error

--- app/matrix/page.tsx
+++ app/matrix/page.tsx
@@ -84,7 +84,12 @@ export default function MatrixPage({}: Props) {
   return (
     <>
       <div
-        style={{ flexDirection: window.innerWidth < 767 ? "column" : "row" }}
+        style={{
+          flexDirection:
+            typeof window !== undefined && window.innerWidth < 767
+              ? "column"
+              : "row",
+        }}
         className="page_container"
       >
         <div className={styles.section}>
