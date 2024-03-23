75454aae2cb033363ac04882f3d95a181f610fe8
Rayyan Alam <rayyanalam650@gmail.com>
2024-03-23 16:21:59 +0530

    style: Styled navbar and fixed navbar for smaller screens

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,15 +1,17 @@
 "use client";
 
-import React, { useState, useEffect } from "react";
-import { usePathname } from "next/navigation";
+import React, { useState, useEffect, useRef } from "react";
 import Link from "next/link";
 import Logo from "@/components/Logo";
 import { LuMenu } from "react-icons/lu";
+import { VscClose } from "react-icons/vsc";
+import useOutsideClick from "@/lib/useOutsideClick";
 
 export default function NavBar() {
-  const pathname = usePathname();
   const isClient = typeof window !== "undefined";
+  const dropDownRef = useRef<HTMLDivElement>(null);
   const [isMobile, setIsMobile] = useState(false);
+  const [showDropDown, setShowDropDown] = useState(false);
   const [scrolled, setScrolled] = useState("");
 
   useEffect(() => {
@@ -41,6 +43,10 @@ export default function NavBar() {
     };
   }, [isClient]);
 
+  useOutsideClick(dropDownRef, () => {
+    setShowDropDown(false);
+  });
+
   return (
     <div className={`navbar_wrapper ${scrolled}`}>
       <div className="navbar_wrapper_logo">
@@ -50,71 +56,92 @@ export default function NavBar() {
       </div>
       {isMobile ? (
         <div className="navbar_wrapper_menu">
-          <LuMenu size={"2rem"} className="menu_logo" />
+          <LuMenu
+            onClick={() => setShowDropDown(true)}
+            size={"2rem"}
+            className="menu_logo"
+          />
+          <div
+            style={{ display: showDropDown ? "block" : "none" }}
+            className="navbar_wrapper_bg"
+          >
+            <div ref={dropDownRef} className="navbar_wrapper_list">
+              <div className="navbar_wrapper_header">
+                <Link href={"/"}>
+                  <Logo />
+                </Link>
+                <VscClose
+                  onClick={() => setShowDropDown(false)}
+                  size={"2.5rem"}
+                />
+              </div>
+              <div
+                onClick={() => setShowDropDown(false)}
+                className="navbar_wrapper_list_item"
+              >
+                <Link href={"/matrix"}>Matrix</Link>
+              </div>
+              <div
+                onClick={() => setShowDropDown(false)}
+                className="navbar_wrapper_list_item"
+              >
+                <Link href={"/geometry"}>Geometry</Link>
+              </div>
+              <div
+                onClick={() => setShowDropDown(false)}
+                className="navbar_wrapper_list_item"
+              >
+                <Link href={"/equation-solver"}>Equation</Link>
+              </div>
+              <div
+                onClick={() => setShowDropDown(false)}
+                className="navbar_wrapper_list_item"
+              >
+                <Link href={"/equation-solver/simultaneous"}>Simultaneous</Link>
+              </div>
+            </div>
+          </div>
         </div>
       ) : (
-        <ul className="navbar_wrapper_list">
-          <li
-            className={`navbar_wrapper_item ${
-              pathname === "/matrix" ? "navbar_wrapper_item--active" : ""
-            }`}
-          >
-            <Link
-              className={`link ${pathname === "/matrix" ? "link-active" : ""}`}
-              href={"/matrix"}
+        <div className="navbar_wrapper_links">
+          <div ref={dropDownRef} className="navbar_dropdown">
+            <button
+              className="navbar_dropdown_btn"
+              onClick={() => setShowDropDown((v) => !v)}
             >
-              Matrix Calculator
-            </Link>
-          </li>
-          <li
-            className={`navbar_wrapper_item ${
-              pathname === "/equation-solver"
-                ? "navbar_wrapper_item--active"
-                : ""
-            }`}
-          >
-            <Link
-              className={`link ${
-                pathname === "/equation-solver" ? "link-active" : ""
-              }`}
-              href={"/equation-solver"}
+              Operations
+            </button>
+            <div
+              style={{ display: showDropDown ? "flex" : "none" }}
+              className="navbar_dropdown_list"
             >
-              Equation Solver
-            </Link>
-          </li>
-          <li
-            className={`navbar_wrapper_item ${
-              pathname === "/geometry" ? "navbar_wrapper_item--active" : ""
-            }`}
-          >
-            <Link
-              className={`link ${
-                pathname === "/geometry" ? "link-active" : ""
-              }`}
-              href={"/geometry"}
-            >
-              Geometry
-            </Link>
-          </li>
-          <li
-            className={`navbar_wrapper_item ${
-              pathname === "/equation-solver/simultaneous"
-                ? "navbar_wrapper_item--active"
-                : ""
-            }`}
-          >
-            <Link
-              className={`link ${
-                pathname === "/equation-solver/simultaneous"
-                  ? "link-active"
-                  : ""
-              }`}
-              href={"/equation-solver/simultaneous"}
-            >
-              Simultaneous Equation
-            </Link>
-          </li>
-        </ul>
+              <div
+                onClick={() => setShowDropDown(false)}
+                className="navbar_dropdown_list_item"
+              >
+                <Link href={"/matrix"}>Matrix</Link>
+              </div>
+              <div
+                onClick={() => setShowDropDown(false)}
+                className="navbar_dropdown_list_item"
+              >
+                <Link href={"/geometry"}>Geometry</Link>
+              </div>
+              <div
+                onClick={() => setShowDropDown(false)}
+                className="navbar_dropdown_list_item"
+              >
+                <Link href={"/equation-solver"}>Equation</Link>
+              </div>
+              <div
+                onClick={() => setShowDropDown(false)}
+                className="navbar_dropdown_list_item"
+              >
+                <Link href={"/equation-solver/simultaneous"}>Simultaneous</Link>
+              </div>
+            </div>
+          </div>
+        </div>
       )}
     </div>
   );
